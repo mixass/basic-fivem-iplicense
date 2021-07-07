cebdb12846e5fec9b72eb6715803ec4e0ba8953b
mixass <67761405+mixass@users.noreply.github.com>
2021-07-07 13:35:14 +0300

    Update license.lua

--- license.lua
+++ license.lua
@@ -1,15 +1,16 @@
 PerformHttpRequest('https://ip-check.online/myip.php', function(err, text, headers)
-  if text == 'licenseip is here' then
-    print('^2 Successfully ^0')
-    --Your code here
-        --Your code here
-            --Your code here
-                --Your code here
-                    --Your code here
-                        --Your code here
-  else
+  if text == 1 or '1' then
     print('^1 Unauthorized access ^0')
     Wait(12000)
     os.exit()
-  end
+  else
+        if text == 'licenseip is here' then
+        print('^2 Successfully ^0')
+        --Your code here
+      else
+        print('^1 Unauthorized access ^0')
+        Wait(12000)
+        os.exit()
+      end
+   end
 end, 'GET', "")
